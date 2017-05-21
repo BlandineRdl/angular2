bcee57e2a6ea90a719f20ec0756b2f003f4e7f34
BlandineRdl <blandineapo@gmail.com>
2017-05-21 18:22:34 +0200

    creation and management of the form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,16 +10,20 @@ import { HomeModule }  from './home/home.module';
 import { HomeComponent }  from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { HikeDetailsComponent } from "./hike/hike-details.component";
+import { ContactModule } from './contact/contact.module';
+import { ContactUsComponent } from './contact/contact-us.component';
 
 @NgModule({
   imports:      [ BrowserModule,
                   HikeModule,
                   HttpModule,
                   HomeModule,
+                  ContactModule,
                   RouterModule.forRoot([
                     { path: 'home', component: HomeComponent },
                     { path: 'hikes', component: HikeListComponent },
                     { path: 'hikes/:id', component: HikeDetailsComponent },
+                    { path: 'contact', component: ContactUsComponent },
                     { path: '', redirectTo: 'home', pathMatch: 'full'},
                     { path: '**', component: PageNotFoundComponent }
                   ]) ],
